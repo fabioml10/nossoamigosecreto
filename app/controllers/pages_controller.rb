fefbdafd3dc5cf4317a4b81b3d5dcd0e13217b06
Fabio Muller <fabioml10@yahoo.com.br>
2020-01-28 21:39:30 -0300

    redirect if user have campaigns

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
+    if current_user
+      if current_user.campaigns.all.size > 0
+        redirect_to campaigns_path
+      end
+    end
   end
 end
